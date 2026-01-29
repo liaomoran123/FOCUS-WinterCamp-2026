f6fa5f1ff817a40c112a20c28d797b4576e31874
liaomoran123 <6199919@qq.com>
2026-01-30 04:33:23 +0800

    Update and rename hello.py to hello world.py

---
+++ hello world.py
@@ -0,0 +1,8 @@
+# Hello World 脚本
+def print_hello():
+    print("👋 Hello World! 这是我的Python脚本～")
+    
+
+if __name__ == "__main__":
+    print_hello()
+    input("\n按任意键关闭窗口...")  
--- hello.py
+++
@@ -1,9 +0,0 @@
-# Hello World 脚本（Python版）
-def print_hello():
-    print("👋 Hello World! 这是我的Python脚本～")
-    print("📌 脚本提交到GitHub仓库，配合Pages页面完成任务～")
-
-if __name__ == "__main__":
-    print_hello()
-    # 关键：加这行，让窗口等待用户按任意键再关闭
-    input("\n按任意键关闭窗口...")  # Windows/Mac/Linux都适用
